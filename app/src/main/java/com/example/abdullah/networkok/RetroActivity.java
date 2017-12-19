c526ea42e1bff93c41b02cdd31fdb109771d4139
abdullah047 <abdullah.asif1995@gmail.com>
2017-12-19 23:30:23 +0500

    Initial commit

---
+++ app/src/main/java/com/example/abdullah/networkok/RetroActivity.java
@@ -0,0 +1,13 @@
+package com.example.abdullah.networkok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RetroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retro);
+    }
+}
